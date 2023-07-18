733165d7487655bdc2bab7d011bf8c9562a088d1
Shane Jonas <jonas.shane@gmail.com>
2023-07-18 13:51:33 -0400

    Updated to latest core previews

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4053,7 +4053,6 @@ export default class MetamaskController extends EventEmitter {
     // setup json rpc engine stack
     const engine = new JsonRpcEngine();
 
-
     // set chainid for domain if not exist
     if (
       this.selectedNetworkController.getChainForDomain(origin) === undefined
@@ -4077,6 +4076,7 @@ export default class MetamaskController extends EventEmitter {
 
     const chainIdForRequest =
       this.selectedNetworkController.getChainForDomain(origin);
+
     console.log('chainIdForRequest', chainIdForRequest);
     console.log(
       'getNetworkClientsById',
@@ -4094,13 +4094,6 @@ export default class MetamaskController extends EventEmitter {
 
     const networkClient =
       this.selectedNetworkController.getClientForDomain(origin);
-
-    console.log(
-      'chainForDomain',
-      origin,
-      'is ',
-      this.selectedNetworkController.getChainForDomain(origin),
-    );
 
     // append origin to each request
     engine.push(createOriginMiddleware({ origin }));
@@ -4161,12 +4154,7 @@ export default class MetamaskController extends EventEmitter {
         }),
       );
     }
-    engine.push(
-      createAsyncMiddleware(async (req, res, next) => {
-       console.log('LAST IN MIDDLEWARE2', req, networkClient.configuration, networkClient);
-       next();
-      }),
-    );
+
     // Unrestricted/permissionless RPC method implementations
     engine.push(
       createMethodMiddleware({
